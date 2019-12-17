7bda0e11c5f793737daa13e9d23e17626e94fbad
Benjamin Marty <info@benjaminmarty.ch>
2019-12-17 09:43:42 +0100

    add to filter on wildcard mp4 for download detection

--- runner.php
+++ runner.php
@@ -9,7 +9,7 @@ $teleboy->login();
 foreach($teleboy->recordings() as $recording) {
     $file_prefix = substr($recording->begin, 0, -14)."_".$recording->station_label."_".$recording->slug."_".str_replace(' ', '_', $recording->subtitle);
 
-    if(!file_exists($file_prefix.".mp4")) {
+    if(!glob($file_prefix."*.mp4")) {
         file_put_contents($file_prefix.".json", json_encode($recording));
         file_put_contents($file_prefix.".mp4", fopen($teleboy->recording_download_url($recording->id), 'r'));
     }
